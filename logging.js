db2e2619e98a5103644df39686e3edbd02327a34
Peter <peter.siegesmund@gmail.com>
2015-08-03 18:56:00 -0400

    refactor

---
+++ logging.js
@@ -0,0 +1,22 @@
+if(Meteor.isServer) {
+  var winston = Npm.require('winston');
+  Npm.require('winston-loggly');
+
+  winston.add(winston.transports.Loggly, {
+    inputToken: process.env.LOGGLY,
+    subdomain: "houndfish",
+    tags: ["Edgar"],
+    json:true
+  });
+
+  Edgar.winston = winston;
+  Edgar.log = winston.log;
+
+  var cronLogger = function (opts) {
+    winston.log(opts.level, opts.message);
+  };
+
+  SyncedCron.config({
+    logger: cronLogger
+  });
+}
